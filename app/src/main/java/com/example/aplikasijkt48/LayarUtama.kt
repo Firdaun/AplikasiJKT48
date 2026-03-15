eb8fcfd8d689e2cea36505965af2ae4204246a23
Firdaun <ketuakls776@gmail.com>
2026-03-15 17:16:03 +0700

    Update UI LayarUtama: Implementasi profil pada TopAppBar dan penyesuaian styling ProfilMember

--- app/src/main/java/com/example/aplikasijkt48/LayarUtama.kt
+++ app/src/main/java/com/example/aplikasijkt48/LayarUtama.kt
@@ -1,18 +1,30 @@
 package com.example.aplikasijkt48
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.aplikasijkt48.ui.theme.AplikasiJKT48Theme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -22,18 +34,54 @@ fun DesainLayarUtama() {
         modifier = Modifier.fillMaxSize(),
         topBar = {
             TopAppBar(
-                title = { Text(text = "Application JKT48", color = Color.White) },
+                title = {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_launcher_background),
+                            contentDescription = "Photo Profile",
+                            modifier = Modifier
+                                .size(40.dp)
+                                .clip(CircleShape)
+                        )
+                        Column(modifier = Modifier.padding(horizontal = 15.dp)) {
+                            Text(
+                                text = "Fahrul Keren",
+                                fontSize = 17.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.White,
+                                style = TextStyle(
+                                    platformStyle = PlatformTextStyle(
+                                        includeFontPadding = false
+                                    )
+                                )
+                            )
+                            Text(
+                                text = "Jacqueline",
+                                color = Color.White,
+                                fontSize = 13.sp,
+                                style = TextStyle(
+                                    platformStyle = PlatformTextStyle(
+                                        includeFontPadding = false
+                                    )
+                                )
+                            )
+                        }
+                    }
+                },
                 colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = Color.Blue
+                    containerColor = Color.Gray
                 )
             )
         }
     ) { innerPadding ->
-        Column(
+        Row(
             modifier = Modifier
                 .padding(innerPadding)
-                .fillMaxSize()
-                .background(Color.LightGray)
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.Start
+
         ) {
             ProfilMember(
                 nama = "Freya Jayawardana",
--- app/src/main/java/com/example/aplikasijkt48/ProfilMember.kt
+++ app/src/main/java/com/example/aplikasijkt48/ProfilMember.kt
@@ -1,13 +1,22 @@
 package com.example.aplikasijkt48
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.aplikasijkt48.ui.theme.AplikasiJKT48Theme
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ProfilMember(nama: String, hobi: String, generasi: String, modifier: Modifier = Modifier) {
@@ -22,7 +31,29 @@ fun ProfilMember(nama: String, hobi: String, generasi: String, modifier: Modifie
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-        AplikasiJKT48Theme {
-            ProfilMember(nama = "Freya Jayawardana", hobi = "menari", generasi = "7")
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Photo Profile",
+            modifier = Modifier
+                .size(60.dp)
+                .clip(CircleShape)
+        )
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = "Fahrul Keren",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Red
+            )
+            Text(text = "Jacqueline")
+
         }
+
+    }
 }
