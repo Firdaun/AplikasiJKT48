7ca33bc2fe52ebb027d331063af7c614da659cf9
Firdaun <ketuakls776@gmail.com>
2026-03-16 19:33:09 +0700

    Update UI LayarUtama: Implementasi tombol interaksi dengan fitur Toast notification

--- app/src/main/java/com/example/aplikasijkt48/LayarUtama.kt
+++ app/src/main/java/com/example/aplikasijkt48/LayarUtama.kt
@@ -1,14 +1,19 @@
 package com.example.aplikasijkt48
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -19,6 +24,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
@@ -31,6 +37,7 @@ import com.example.aplikasijkt48.ui.theme.AplikasiJKT48Theme
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DesainLayarUtama() {
+    val context = LocalContext.current
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
@@ -102,10 +109,32 @@ fun DesainLayarUtama() {
                 painter = painterResource(id = R.drawable.fritzy_dimalamhari), // Sesuaikan id gambar
                 contentDescription = "Foto Bersama",
                 modifier = Modifier
-                    .padding(start = 50.dp, end = 50.dp)
+                    .padding(start = 50.dp, end = 50.dp, bottom = 50.dp)
                     .clip(RoundedCornerShape(20.dp))
 
             )
+            Button(
+                onClick = { Toast.makeText(context, "Hello Fahrul! Fritzy mewaro kamu loh \uD83D \uDC4B", Toast.LENGTH_SHORT).show()},
+                modifier = Modifier
+                    .fillMaxWidth(0.6f)
+                    .height(50.dp),
+                shape = RoundedCornerShape(25.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF2883E5),
+                    contentColor = Color.White
+                ),
+                elevation = ButtonDefaults.buttonElevation(
+                    defaultElevation = 6.dp,
+                    pressedElevation = 2.dp
+                )
+            ) {
+                Text(
+                    text = "Interact",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    letterSpacing = 1.sp
+                )
+            }
         }
     }
 }
